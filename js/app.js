bdfdcbdd49322f73d42e05e43d1e4d0386ffce87
David Park <djp22@columbia.edu>
2014-09-12 11:41:08 -0400

    Working baseline.

--- js/app.js
+++ js/app.js
@@ -2,13 +2,41 @@
 var app = angular.module('MyDemoApp', ['ngRoute']);
 
 //set up routing
+/*
 app.config(['$routeProvider', function($routeProvider){
-	$routeProvider.otherwise({
+	$routeProvider
+                .when("/", {
+                    controller: "HomeCtrl",
+                    templateUrl: "partials/home.html"
+        })
+            .otherwise({
 		redirectTo: "/"
 	});
 }]);
-
+*/
 // add a controller
 app.controller('DemoCtrl', ['$scope', function($scope){
-	
+        /* simple text property */
+	$scope.message = "Hello, world!";
+        
+        /* collection for ng-repeat */
+        $scope.developers = [
+            {name: "David", title: "Sr. Director" },
+            {name: "Ashkan", title: "Lead Developer" },
+            {name: "Elizabeth", title: "Front-end Maven"}
+        ];
+        
+        /* Add method will go here */
+}]);
+
+app.controller('HomeCtrl', ['$scope', function($scope){
+        $scope.message = "Hello, world!";
+}]);
+
+app.controller('ListCtrl', ['$scope', function($scope){
+        $scope.developers = [
+            {name: "David", title: "Sr. Director" },
+            {name: "Ashkan", title: "Lead Developer" },
+            {name: "Elizabeth", title: "Front-end Maven"}
+        ];
 }]);
